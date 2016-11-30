4e03d7573cb24d0119a9bf846572980d37ca1b08
Agustin Barto <abarto@machinalis.com>
2016-11-30 18:49:03 -0300

    Switch to Flask-Security login view. Customize default login template.

--- movie_recommendations/views.py
+++ movie_recommendations/views.py
@@ -12,11 +12,6 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
-def login():
-    return render_template('login.html')
-
-
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
