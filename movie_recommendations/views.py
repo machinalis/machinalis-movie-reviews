a5c19df75ab25c299669f6b71913d0ab165cf55f
Agustin Barto <abarto@machinalis.com>
2016-12-01 08:42:10 -0300

    Restrict access to existing views. Update templates.

--- movie_recommendations/views.py
+++ movie_recommendations/views.py
@@ -3,15 +3,18 @@ movie_recommendations views
 """
 
 from flask import render_template
+from flask_security import login_required
 
 from movie_recommendations import app
 
 
 @app.route('/')
+@login_required
 def index():
     return render_template('index.html')
 
 
 @app.route('/profile')
+@login_required
 def profile():
     return render_template('profile.html')
